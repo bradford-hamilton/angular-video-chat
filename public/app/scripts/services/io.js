e17f8bb85b86e61ceb9706ea23bd49221a7bc58d
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-09-14 17:20:27 -0600

    initial commit, server and some angular complete

---
+++ public/app/scripts/services/io.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name publicApp.Io
+ * @description
+ * # Io
+ * Factory in the publicApp.
+ */
+
+angular.module('publicApp')
+  .factory('Io', function () {
+    if (typeof io === 'undefined') {
+      throw new Error('Socket.io required');
+    }
+    return io;
+  });
